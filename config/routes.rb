622e7d5fcb9958ea5952bfbf0046696c4991dc61
Chinedu Arinze <c.s.arinze@gmail.com>
2020-01-16 14:59:04 -0500

    Add Posts controller, show file in posts folder and resources path in routes.rb file. All test are passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'posts/:id', to: 'posts#show'
+  # the above and below code both work but the conventional rails method is to use the resources method
+  # to accomplish this.
+  resources :posts, only: :show
 end
